58307dba2a57f55f0a61ee660ac8c1c5e1784928
DariusZdroba <darius-beniamin.zdroba@snyk.io>
2024-10-22 15:22:16 +0300

    chore: change file

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -1,9 +1,10 @@
 package handlers
 
 import (
+	"goapi/internal/middleware"
+
 	"github.com/go-chi/chi"
 	chimiddle "github.com/go-chi/chi/middleware"
-	"goapi/internal/middleware"
 )
 
 func Handler(r *chi.Mux) {
@@ -15,6 +16,6 @@ func Handler(r *chi.Mux) {
 		// Middleware for /account route to authorize access
 		router.Use(middleware.Authorization)
 
-		router.Get("/coins", GetCoinBallance)
+		router.Get("/coinballance", GetCoinBallance)
 	})
 }
